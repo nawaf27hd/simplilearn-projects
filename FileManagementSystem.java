b303f75b702025515435d0803a2add0d38e312c4
nawaf27hd <rock.lee321@hotmail.com>
2021-06-16 22:12:06 +0300

    first commit

---
+++ FileManagementSystem.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FileManagementSystem {
+	
+
+	public static void main(String[] args) {
+
+	
+	MyMenu menu1 = new MyMenu();
+	menu1.Menu();
+	TheList list1 = new TheList();
+	list1.list();
+		
+	}
+	        
+	        
+	        
+	
+
+	}
+
+
+
